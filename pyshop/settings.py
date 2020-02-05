ef85e6bd3f26f1ea29c0e9cb1ae12e4f661da6ee
abedkiloo <abedxh@gmail.com>
2020-02-05 10:16:29 +0300

    update the view  of products page

--- pyshop/settings.py
+++ pyshop/settings.py
@@ -53,7 +53,7 @@ ROOT_URLCONF = 'pyshop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join('templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

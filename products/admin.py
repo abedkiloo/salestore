7a736aa0ab9285daacefaef79a58e001d6237862
abedkiloo <abedxh@gmail.com>
2020-02-05 09:47:49 +0300

    added first view of products

--- products/admin.py
+++ products/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from .models import Products
+from .models import Offer
 
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name', 'price', 'stock']
+    list_display = ('name', 'price', 'stock')
 
 
-admin.site.register(Products,ProductAdmin)
+class OffersAdmin(admin.ModelAdmin):
+    list_display = ('code', 'description', 'discount')
+
+
+admin.site.register(Products, ProductAdmin)
+admin.site.register(Offer, OffersAdmin)
--- products/views.py
+++ products/views.py
@@ -1,12 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Products
 
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Index page")
+    products = Products.objects.all()
+    return render(request, 'index.html', {'products': products})
 
 
 def new_product(request):
